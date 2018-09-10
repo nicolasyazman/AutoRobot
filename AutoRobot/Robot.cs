041355730f2beee7fd282710a3fb9276924e028e
Peuch <lucas.78.13@gmail.com>
2018-09-10 17:49:24 +0200

    add betweenPoint test + fct

--- AutoRobot/Robot.cs
+++ AutoRobot/Robot.cs
@@ -27,5 +27,28 @@ namespace AutoRobot
             double yToMove = Math.Sin(angle) * distance;
             return MoveRobotCartesian(xToMove, yToMove);
         }
+
+        public List<Point2D> CalculateIntermediatePointsBetween2Points(Point2D origin, Point2D dest, double nbPoints)
+        {
+            double dx = dest.X - origin.X;
+            double dy = dest.Y - origin.Y;
+
+            double distance              = Math.Sqrt(dx * dx + dy * dy);
+            double distanceBetweenPoints = distance / (nbPoints + 1);
+            double angle = Point2D.AbsoluteBearing(origin, dest);
+
+            List<Point2D> res = new List<Point2D>();
+            for(int i=0; i<nbPoints; i++)
+            {
+                Point2D pointIntermediate = new Point2D(
+                    origin.X + Math.Cos(angle) * (distanceBetweenPoints * (i+1)),
+                    origin.Y + Math.Sin(angle) * (distanceBetweenPoints * (i + 1))
+                );
+
+                res.Add(pointIntermediate);
+            }
+
+            return res;
+        }
     }
 }
--- AutoRobotTests/Program.cs
+++ AutoRobotTests/Program.cs
@@ -4,6 +4,7 @@ using NUnit;
 using AutoRobot;
 using NUnit.Framework;
 using FluentAssertions;
+using System.Collections.Generic;
 
 namespace AutoRobotTests
 {
@@ -63,6 +64,41 @@ namespace AutoRobotTests
 
             r.Position.X.Should().BeApproximately(x2, 0.001);
             r.Position.Y.Should().BeApproximately(y2, 0.001);
+        }
+
+        //prend un point d origine et de destination et un nombre de pts intermediaires et calcul la posi
+        //position de ces points sur la ligne
+
+        [TestCase]
+        public static void TestCalculIntermediatePoint()
+        {
+            Random rand;
+            double x1, y1, x2, y2;
+
+            rand = new Random();
+            x1 = rand.NextDouble();
+            y1 = rand.NextDouble();
+            x2 = rand.NextDouble();
+            y2 = rand.NextDouble();
+
+            Point2D orig = new Point2D(x1, y1);
+            Point2D dest = new Point2D(x2, y2);
+
+            double angle = Point2D.AbsoluteBearing(orig, dest);
+            Robot r = new Robot(orig);
+
+            int nbPoints = rand.Next(3, 10);
+            List<Point2D> intermediatePoints = r.CalculateIntermediatePointsBetween2Points(orig, dest, nbPoints);
+
+            for (int i = 0; i < nbPoints; i++)
+            {
+                Point2D.AbsoluteBearing(orig, intermediatePoints[i]).Should().BeApproximately(angle, 0.001);
+                double distance = Math.Sqrt((intermediatePoints[i].X - orig.X)
+                    * (intermediatePoints[i].X - orig.X)
+                    + (intermediatePoints[i].Y - orig.Y)
+                    * (intermediatePoints[i].Y - orig.Y)
+                );
+            }
         }
     }
 }
